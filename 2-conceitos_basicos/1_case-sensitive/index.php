e28d8b7b91159c6f8614c90b119e536335909570
Ismael Hofmam <ismaeloh1982@gmail.com>
2023-09-10 10:00:26 -0300

    5ª alteração

---
+++ 2-conceitos_basicos/1_case-sensitive/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Não é case sensitive
+echo "teste <br>";
+
+echo "testando 2 <br>"; //eChO
+
+echo "testando 3 <br>"; //ECHO
+
+//É case sensitive
+$nome = "Ismael";
+$NOME = "Teste";
+
+echo $nome;
+echo "<br>";
+echo $NOME;
