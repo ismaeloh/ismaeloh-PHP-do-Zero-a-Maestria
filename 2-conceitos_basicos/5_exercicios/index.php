b938f16064cbcb6a3875df4cce6e72a1b9ffecb6
Ismael OH <110628422+ismaeloh@users.noreply.github.com>
2023-09-18 16:35:50 -0300

    9ª alteração

---
+++ 2-conceitos_basicos/5_exercicios/index.php
@@ -0,0 +1,15 @@
+<?php
+
+// Esta variável contém o nome da pessoa
+
+$nome = "Ismael";
+
+// Esta variável contém a idade da pessoa
+
+$idade = 41;
+
+// Esta variável contém a profissão da pessoa
+
+$profissao = "Programador Half-boiled";
+
+echo $profissao;
