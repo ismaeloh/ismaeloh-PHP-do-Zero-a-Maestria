b0074f757e424dab39bbaed57960e405c033dbb8
Ismael Hofmam <ismaeloh1982@gmail.com>
2023-09-23 19:02:59 -0300

    19ª alteração

---
+++ 3_tipos_de_dados/9_exercicio_5/index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "O Madruguinha é um cachorro gordo <br>"; //aspas duplas
+echo 'O Madruguinha é um cachorro gordo <br>'; //aspas simples
+
+$nome = "Madruguinha";
+$idade = 8;
+
+echo "Olá, ele é o $nome e tem $idade anos";
