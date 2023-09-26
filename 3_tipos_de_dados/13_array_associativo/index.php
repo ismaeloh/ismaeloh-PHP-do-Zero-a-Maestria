165aa6d1d1bdea2997ecb295b2b2615ef61ee54c
Ismael Hofmam <ismaeloh1982@gmail.com>
2023-09-26 10:04:12 -0300

    23ª alteração

---
+++ 3_tipos_de_dados/13_array_associativo/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$arr = ['nome' => 'Madruguinha', 'patas' => 4, 'cor' => 'Marrom'];
+
+echo $arr['nome'];
+echo "<br>";
+print_r($arr);
+echo "<br>";
+echo $arr['patas'];
+echo "<br>";
+
+$arrAssoc = ['chave' => 'valor', 'bool' => true];
+echo "<br>";
+print_r($arrAssoc);
